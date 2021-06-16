6d54d54957150237baecd057b008dea6fd8d70dd
Zharvis <fatih.mehmed.cin@gmail.com>
2021-06-16 15:30:47 +0200

    added usernames

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 import socket 
 from _thread import start_new_thread
+from time import sleep
     
 # socket anlegen
-host = '127.0.0.1'
+host = '192.168.178.9'
 port = 5555
 sock = socket.socket()
 try:
     sock.connect((host, port))
+    username_is_invalid = 1
+    while username_is_invalid:
+        username = input("Input an username. (max. 16 characters)\n")
+        sock.send(username.encode())
+        username_is_invalid = int(sock.recv(1024).decode())
+        if username_is_invalid:
+            print("Username is already taken. Choose another name.")
 except ConnectionRefusedError:
     print("Cannot reach server. Closing...")
     exit()
+
 
 def recv_msg():
     try:
@@ -21,6 +30,7 @@ def recv_msg():
         exit()
 
 start_new_thread(recv_msg, ())
+sleep(1)
 
 try:
     while True: #Sendeschleife
--- server.py
+++ server.py
@@ -2,14 +2,13 @@ import socket
 from _thread import start_new_thread
 from datetime import datetime
 
-host = '127.0.0.1'
+host = '192.168.178.9'
 port = 5555
 #socket anlegen
 sock = socket.socket()
 sock.bind((host, port))
 sock.listen(1)
-clients_list = []
-
+active_clients = dict()
 print(f"Server({host}) listening on port {port}")
 server_msg = f"New session started at {datetime.now().strftime('%d.%m.%Y - %H:%M:%S')}"
 print(server_msg)
@@ -17,47 +16,61 @@ with open('history.txt', 'a') as text_file:
     text_file.write(server_msg + '\n')
 
 def handle_client(conn, addr):
-    print(f"{addr[0]} connected.")
     try:
+        username = ""
+        username_is_invalid = 1
+        while username_is_invalid:
+            username = conn.recv(16).decode()
+            if username not in active_clients:
+                username_is_invalid = 0
+                conn.send("0".encode())
+            else:
+                conn.send("1".encode())
+            
+        print(f"{username} connected.")
         with open('history.txt', 'r') as text_file:
                 conn.send(text_file.read().encode())
-        conn.send("Welcome to the Server.".encode())
+        conn.send("Welcome to the Server.\n".encode())
+        conn.send(f"Type '-p [address] [YOUR_MESSAGE]' to send private messages.".encode())
         # Client wird der Clientliste hinzugefuegt
-        clients_list.append(conn)
+        active_clients[username] = conn
         while True: # Empfangsschleife
             # Warten auf Nachrichten der Clientseite
             msg = conn.recv(1024)
             # Erfassen der Empfangszeit
             now = datetime.now().strftime("%H:%M")
+            # Testen ob -private Modifier benutzt wurde
+            if msg[:9] is "-private":
+                pass
             # Ausgabe der Clientnachricht in der Serverkonsole
-            msg = f"[{addr[0]} - {now}]: {msg.decode()}"
+            msg = f"[{username} - {now}]: {msg.decode()}"
             print(msg)
             # Weiterleiten der Nachricht an andere Clients
-            broadcast(msg, conn)
+            broadcast(msg, username)
             # Protokolierung des Chatverlaufs
             with open('history.txt', 'a') as text_file:
                 text_file.write(msg + '\n')
     except ConnectionResetError:
-        clients_list.remove(conn)
-        print(f"{addr[0]} disconnected.")
-        broadcast(f"{addr[0]} disconnected.")
+        del active_clients[username]
+        print(f"{username} disconnected.")
+        broadcast(f"{username} disconnected.")
 
-def broadcast(msg, conn=None):
-    if conn is None:
-        for client in clients_list:
+def broadcast(msg, username=None):
+    if username is None:
+        for clientname, conn in active_clients.items():
             try:
-                client.send(msg.encode())
+                conn.send(msg.encode())
             except  ConnectionResetError:
-                print(f"{addr[0]} disconnected.")
-                clients_list.remove(conn)
+                print(f"{clientname} disconnected.")
+                del active_clients[username]
     else:
-        for client in clients_list:
-            if client is not conn:
+        for clientname, conn in active_clients.items():
+            if clientname is not username:
                 try:
-                    client.send(msg.encode())
+                    conn.send(msg.encode())
                 except  ConnectionResetError:
-                    print(f"{addr[0]} disconnected.")
-                    clients_list.remove(conn)
+                    print(f"{clientname} disconnected.")
+                    del active_clients[username]
 
 while True: #Serverschleife 
     # Auf neuen Client warten
